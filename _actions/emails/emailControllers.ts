18f9ced0d8bb5819da5306f4095efd45e974cfef
xmarya <marya_hy@hotmail.com>
2025-03-22 16:54:09 +0300

    simple changes

--- _actions/emails/emailControllers.ts
+++ _actions/emails/emailControllers.ts
@@ -6,18 +6,21 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_KEY);
 
-export async function sendWelcome() {
+export async function sendWelcome(username:string) {
      try {
-         const { data } = await resend.emails.send({
+         const { data, error } = await resend.emails.send({
            from: 'Acme <onboarding@resend.dev>',
-           to: ['delivered@resend.dev'],
-           subject: 'Hello world',
-           react: EmailTemplate({ firstName: 'John', message: "it's a Resend Email" }),
+           to: "shhmmanager1@gmail.com",
+           subject: 'Signup Successfully!',
+           react: EmailTemplate({ message: `HI ${username}, Welcome to SAHM platform` }),
          });
+
+         if(error) console.log("ERROR🔴 SEND EMAIL", error);
      
          return {success: true, data};
      
        } catch (error) {
+        console.log("ERROR🔴 SEND EMAIL");
          return {success:false, error};
        }
 
