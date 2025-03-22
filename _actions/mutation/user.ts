42e7e2872a922cd8cdd84e24f3c85e22de61408a
xmarya <marya_hy@hotmail.com>
2025-03-22 16:53:23 +0300

    edits on the creatediscorduser

--- _actions/mutation/user.ts
+++ _actions/mutation/user.ts
@@ -1,15 +1,17 @@
 import { withDBConnection } from "@/_utils/controllerWrapper";
 import User from "@/models/userModel";
 
-export const createUser = withDBConnection(async (newUser) => {
+export const createDiscordUser = withDBConnection(async (newUser) => {
   console.log("before create");
   const user = await User.create({
     email: newUser.email,
     username: newUser.name,
     image: newUser.image,
+    signMethod: "discord",
+    discord: {id: newUser.id}
     //TODO: distinguish between the sigWay methods
   });
-  console.log("createNewUser🎭", user.username);
+  console.log("createNewUser🎭", user);
   // NOTE: I think there is no need to return the newly created usr because the signIn callback only returns boolean
   // return user;
 });
