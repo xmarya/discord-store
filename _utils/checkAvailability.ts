ff670549a28852b9258766fcd88c44e7148d284e
xmarya <marya_hy@hotmail.com>
2025-03-23 15:35:44 +0300

    forgot to add the file to the last commit

--- _utils/checkAvailability.ts
+++ _utils/checkAvailability.ts
@@ -1,20 +1,67 @@
-import { getField } from "@/_actions/controllerGlobal"
+import { getField } from "@/_actions/controllerGlobal";
 
+export async function isDuplicated(Model: "User" | "Store", field: string, value: string) {
+  const valuesList = await getField(Model, field);
+  const isTaken = valuesList.some((obj) => obj[field] === value);
 
-export async function isDuplicated(Model: "User" | "Store", field:string, value:string) {
-    console.log("iside isDuplicated");
-    const valuesList = await getField(Model, field);
-    const result = valuesList.some((obj) => obj[field] === value);
-
-    return result
+  return isTaken;
 }
+
+type PossiblyDuplicatedData = Array<{
+  Model: "User" | "Store";
+  field: string;
+  values: Array<Record<string,any>>;
+}>;
+
+
 /*
+  arguments:
+    get passed:
     [
-  {
-    _id: new ObjectId('67dea416406d70062346c232'),
-    email: 'user@user.com',
-    planExpiresInDays: 0,
-    id: '67dea416406d70062346c232'
-  }
-]
+        {Model: "", field1: "", value1: [""]},
+        {Model: "", field2: "", value2: [""]},
+    ]
+  example:
+   const data:PossiblyDuplicatedData = [
+        {Model:"User", field:"email username", values: [{email: (inputs.email) as string}, {username: (inputs.username) as string}]},
+    ]
 */
+async function advancedIsDuplicated(data: PossiblyDuplicatedData) {
+  const errors:Array<string> = [];
+  for (const {Model, field, values} of data) {
+    // console.log(values); //[ { email: 'user@user.com' }, { username: 'user12' } ]
+    const valuesList = await getField(Model, field);
+    //console.log(":IST", valuesList);
+    /* :IST [
+        {
+          _id: '67dea416406d70062346c232',
+          email: 'user@user.com',
+          username: 'user1',
+          planExpiresInDays: 0,
+          id: '67dea416406d70062346c232'
+        }
+      ]
+    */
+    field.split(" ").forEach((field, index) => {
+        const isTaken = valuesList.some(obj => obj[field] === values[index][field]);
+        isTaken && errors.push(field);
+    });
+
+    return errors;
+  }
+
+  /*
+        data: [
+            {
+                Model: 'User',
+                field: 'email username',
+                value: [ 'user@user.com', 'user12' ]
+            }
+        ]
+    */
+   // ...data {
+   //     Model: 'User',
+   //     field: 'email username',
+   //     value: [ 'user@user.com', 'user12' ]
+   //   }
+}
