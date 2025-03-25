919e37ff341443439655b7c9082000826dd28e94
xmarya <marya_hy@hotmail.com>
2025-03-25 14:25:22 +0300

    added InputStatusCode

--- _Types/FormContextInput.ts
+++ _Types/FormContextInput.ts
@@ -1,36 +1,35 @@
-import { StatusCode } from "./FormContext";
+import { FormState } from "./FormContext";
+export type InputStatusCode = "valid" | "noChange" | "error"; 
 
 // STEP 1) Create a mapping type with keys hold a specific type.
 export type EventTypesMap = {
-  onBlur: React.FocusEvent<HTMLInputElement>,
-  onKeyDown: React.KeyboardEvent<HTMLInputElement>
-}
+  onBlur: React.FocusEvent<HTMLInputElement>;
+  onKeyDown: React.KeyboardEvent<HTMLInputElement>;
+};
 
 // STEP 2) use keyof to MAP to extract the keys into a separate type.
 export type EventTypes = keyof EventTypesMap;
 
 // STEP 3) use them as a Generic to dynamically resolve the correct type based on the key.
-export type FormContextInput <T extends EventTypes> = {
-  name:string,
-  eventType: T,
-  validate: (event:EventTypesMap[T]) => StatusCode | Promise<StatusCode>
-
-} & React.InputHTMLAttributes<HTMLInputElement> 
+export type FormContextInput<T extends EventTypes> = {
+  name: string;
+  eventType: T;
+  validate: (event: EventTypesMap[T]) =>  Promise<FormState>;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 /* OLD CODE (kept for reference): 
     export type FormContextInput  = {
       name:string,
     } & React.InputHTMLAttributes<HTMLInputElement> &(
-      {eventType: "onBlur", validate: (event:React.FocusEvent<HTMLInputElement>) => StatusCode | Promise<StatusCode>}
+      {eventType: "onBlur", validate: (event:React.FocusEvent<HTMLInputElement>) => InputStatusCode | Promise<InputStatusCode>}
       |
-      {eventType: "onKeyDown", validate: (event:React.KeyboardEvent<HTMLInputElement>) => StatusCode | Promise<StatusCode>}
+      {eventType: "onKeyDown", validate: (event:React.KeyboardEvent<HTMLInputElement>) => InputStatusCode | Promise<InputStatusCode>}
     );
 */
 
-
 /*
   The current FormContextInput type includes eventType and validate as separate properties, 
-  but they are not linked. So TypeScript can't enforce that when eventType is 'onBlur', validate should take a FocusEvent.
+  since they are not linked TypeScript can't enforce that when eventType is 'onBlur', validate should take a FocusEvent.
   That's probably the root of the issue.
   The solution would be to create a discriminated union 
   where eventType and the event type in validate are connected. 
