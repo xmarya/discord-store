350036021ba114ed8237432d7fb862dc36aa5b88
xmarya <marya_hy@hotmail.com>
2025-03-26 20:07:55 +0300

    removed unused import

--- _actions/mutation/store.ts
+++ _actions/mutation/store.ts
@@ -2,23 +2,21 @@
 
 import { auth } from "@/_config/auth";
 import { withDBConnection } from "@/_utils/controllerWrapper";
-import { StoreSchema } from "@/_utils/ZodValidations/storeSchema";
 import Store from "@/models/storeModel";
 import User from "@/models/userModel";
 import { revalidatePath } from "next/cache";
 
-export const createStore = withDBConnection(async (prevState:any, formData: FormData) => {
+export const createStore = withDBConnection(async (prevState: any, formData: FormData) => {
   // the newStore probably if got from the formData, the userId from the session
   const session = await auth();
   const userId = session?.user.id;
-  const {storeName, logo} = Object.fromEntries(formData);
+  const { storeName, logo } = Object.fromEntries(formData);
 
   // STEP 1) create the new store:
   const newStore = await Store.create({
     // storeName: formData.get("storeName"),
     storeName,
     owner: userId,
-    
   });
 
   // STEP 2) link it with the user using userId:
@@ -28,21 +26,20 @@ export const createStore = withDBConnection(async (prevState:any, formData: Form
   revalidatePath("/dashboard/myStore");
 });
 
-export const updateStore = withDBConnection(async (prevState:any, formData: FormData) => {
-  const {storeId, storeName, logo} = Object.fromEntries(formData);
+export const updateStore = withDBConnection(async (prevState: any, formData: FormData) => {
+  const { storeId, storeName, logo } = Object.fromEntries(formData);
 
   // STEP 1) pass in ONLY the editable data:
-    await Store.findByIdAndUpdate(storeId, {
-      storeName,
-    });
-    // STEP 2) revalidate the path
-    revalidatePath("/dashboard/myStore");
-  }
-);
+  await Store.findByIdAndUpdate(storeId, {
+    storeName,
+  });
+  // STEP 2) revalidate the path
+  revalidatePath("/dashboard/myStore");
+});
 
 export const publishStore = withDBConnection(async (storeId) => {
   await Store.findByIdAndUpdate(storeId, {
-    status:"active"
+    status: "active",
   });
 });
 
