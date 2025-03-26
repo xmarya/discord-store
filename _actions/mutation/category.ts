5df663ad40a2e7edbcb82098b3699ecd24922313
xmarya <marya_hy@hotmail.com>
2025-03-26 19:30:54 +0300

    small edits

--- _actions/mutation/category.ts
+++ _actions/mutation/category.ts
@@ -3,33 +3,22 @@
 import { withDBConnection } from "@/_utils/controllerWrapper";
 import { CategoryBasic } from "@/_Types/Category";
 import Category from "@/models/categoryModel";
-import Product from "@/models/productModel";
-import { startSession } from "mongoose";
 
 export const createCategory = withDBConnection(async (category: CategoryBasic, storeId: string) => {
-  const random = Math.random() * 10;
   const newCategory = await Category.create({
     name: category.name,
     colour: category.colour,
     store: storeId,
   });
-
-  await Product.create({
-    name: `product${random}`,
-    price: 14.25,
-    quantity: 4,
-    image: "image.jpeg",
-    category: newCategory._id,
-    description: "description",
-    status: "inStock",
-    store: storeId,
-  });
-
   return JSON.parse(JSON.stringify(newCategory));
 });
 
-export const updateCategory = withDBConnection(async (categoryId, updatedCategory: CategoryBasic) => {
-  const updatedDoc = await Category.findByIdAndUpdate();
+export const updateCategory = withDBConnection(async (categoryId, updatedCategory: Partial<CategoryBasic>) => {
+  const updatedDoc = await Category.findByIdAndUpdate(categoryId, {
+    ...updatedCategory
+  });
+
+  return JSON.parse(JSON.stringify(updatedDoc));
 });
 
 export const deleteCategory = withDBConnection(async (categoryId) => {
