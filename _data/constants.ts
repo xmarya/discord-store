645ccc25c69079e833bc14cdfb3f6ac4c2ddc649
xmarya <marya_hy@hotmail.com>
2025-03-26 19:29:29 +0300

    two constants for file validation were added

--- _data/constants.ts
+++ _data/constants.ts
@@ -1,3 +1,13 @@
 
 
 export const DOCS_PER_PAGE:number = 6;
+
+export const ACCEPTED_IMAGE_TYPES = [
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "image/svg+xml"
+];
+
+export const MAX_IMAGE_SIZE = 1024 * 1024 * 1.5; // converting 1.5MB to KB
+
